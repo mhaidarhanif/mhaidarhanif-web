de639b0b0ef2e5721497c83686250c3537a55a8b
M Haidar Hanif <git@mhaidarhanif.com>
2021-12-28 07:07:53 +0700

    feat: blog page

---
+++ app/routes/blog.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'remix'
+
+import { Container, H, P } from '~/components'
+
+export default function Blog() {
+  return (
+    <Container>
+      <H as="h1">Blog</H>
+      <P>
+        Collection of ideas, stories, thoughts, learnings, and various sharings.
+        Usually around life, career, tech, software engineering, web
+        development, and multiple topics.
+      </P>
+    </Container>
+  )
+}
