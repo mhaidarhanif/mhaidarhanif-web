c325454b92f3977c24e08dcad2dbf117e969ce54
M Haidar Hanif <git@mhaidarhanif.com>
2021-12-28 07:21:12 +0700

    feat: use avatar photo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,14 @@
-import { Container, H } from '~/components'
+import { Container, Avatar, AvatarImage, H } from '~/components'
 
 export default function Index() {
   return (
     <Container>
+      <Avatar size="5">
+        <AvatarImage
+          src="/photos/mhaidarhanif-cyan.jpeg"
+          alt="M Haidar Hanif"
+        />
+      </Avatar>
       <H as="h1">M Haidar Hanif</H>
     </Container>
   )
