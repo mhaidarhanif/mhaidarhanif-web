a2536c2d21159e9da25430617deb668722385549
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-14 03:36:35 +0700

    feat: styled link component

--- app/components/link.tsx
+++ app/components/link.tsx
@@ -1,3 +1,31 @@
-import { Link } from 'remix'
+import { FunctionComponent } from 'react'
+import { Link as RemixLink } from 'remix'
 
-export { Link }
+import { styled } from '~/stitches'
+
+interface LinkProps {
+  css?: any
+  to: string
+  prefetch: any
+  children: React.ReactNode
+}
+
+const LinkStyled = styled(RemixLink, {
+  transition: 'color 0.2s ease',
+  borderRadius: '$2',
+  color: '$anchor',
+  '&:hover': {
+    color: '$anchorHover',
+  },
+})
+
+export const Link: FunctionComponent<LinkProps> = ({
+  css,
+  to,
+  prefetch,
+  children,
+}) => (
+  <LinkStyled css={css} to={to} prefetch={prefetch}>
+    {children}
+  </LinkStyled>
+)
