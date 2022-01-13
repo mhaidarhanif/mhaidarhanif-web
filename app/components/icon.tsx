310409ec82af8544bb457f060b5e93f3c4d889aa
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-14 04:22:10 +0700

    feat: icon with switch function

--- app/components/icon.tsx
+++ app/components/icon.tsx
@@ -20,67 +20,53 @@ import {
   FaYoutube as YouTubeIcon,
   FaTh as PolyworkIcon,
   // Misc
-  FaCat as CatamystIcon,
   FaFileAlt as CVIcon,
+  // Compnay
+  FaCat as CatamystIcon,
+  FaProjectDiagram as KontenbaseIcon,
+  FaCode as CodePolitanIcon,
 } from 'react-icons/fa'
+import { GiClover as HighSkillMastersIcon } from 'react-icons/gi'
+
+const switchFn =
+  (lookupObject: any, defaultCase = '_default') =>
+  (expression: string | number) =>
+    lookupObject[expression] || lookupObject[defaultCase]
+
+export const iconMaps = {
+  // General
+  circle: <CircleIcon />,
+  website: <WebsiteIcon />,
+  // Theme
+  light: <LightIcon />,
+  dark: <DarkIcon />,
+  // Social media
+  twitter: <TwitterIcon />,
+  github: <GitHubIcon />,
+  linkedin: <LinkedInIcon />,
+  instagram: <InstagramIcon />,
+  facebook: <FacebookIcon />,
+  youtube: <YouTubeIcon />,
+  polywork: <PolyworkIcon />,
+  // Chat
+  slack: <SlackIcon />,
+  twist: <TwistIcon />,
+  discord: <DiscordIcon />,
+  // Misc
+  cv: <CVIcon />,
+  // Company
+  catamyst: <CatamystIcon />,
+  kontenbase: <KontenbaseIcon />,
+  codepolitan: <CodePolitanIcon />,
+  highskillmasters: <HighSkillMastersIcon />,
+  // Default
+  default: <CircleIcon />,
+}
+
+const iconSwitch = switchFn(iconMaps, 'default')
 
 interface IconProps {
   name: string
 }
 
-export const Icon: FunctionComponent<IconProps> = ({ name }) => {
-  switch (name) {
-    /**
-     * General
-     */
-    case 'circle':
-      return <CircleIcon />
-    case 'website':
-      return <WebsiteIcon />
-    /**
-     * Theme
-     */
-    case 'light':
-      return <LightIcon />
-    case 'dark':
-      return <DarkIcon />
-    /**
-     * Social media
-     */
-    case 'twitter':
-      return <TwitterIcon />
-    case 'github':
-      return <GitHubIcon />
-    case 'linkedin':
-      return <LinkedInIcon />
-    case 'instagram':
-      return <InstagramIcon />
-    case 'facebook':
-      return <FacebookIcon />
-    case 'youtube':
-      return <YouTubeIcon />
-    case 'polywork':
-      return <PolyworkIcon />
-    /**
-     * Chat
-     */
-    case 'slack':
-      return <SlackIcon />
-    case 'twist':
-      return <TwistIcon />
-    case 'discord':
-      return <DiscordIcon />
-    /**
-     * Misc
-     */
-    case 'catamyst':
-      return <CatamystIcon />
-    case 'cv':
-      return <CVIcon />
-    /**
-     * When nothing found
-     */
-    default:
-      return <CircleIcon />
-  }
-}
+export const Icon: FunctionComponent<IconProps> = ({ name }) => iconSwitch(name)
