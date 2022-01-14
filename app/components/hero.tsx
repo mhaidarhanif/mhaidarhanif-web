8ad2f75780996fe2e0c80da884dd34bacc2bd279
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-14 09:18:43 +0700

    feat: install and try blurhash

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react'
+import { Blurhash } from 'react-blurhash'
 
 import { styled } from '~/stitches'
 
@@ -12,6 +13,7 @@ interface HeroImageProps {
   src: string
   alt: string
   align?: string | 'top' | 'center' | 'bottom' | undefined
+  blurhash?: string
 }
 
 const HeroStyled = styled('div', {
@@ -26,6 +28,12 @@ const HeroImageContainer = styled('div', {
   display: 'flex',
   height: '300px',
   '@desktop': { height: '450px' },
+
+  '& .blurhash': {
+    width: '100%',
+    height: '300px',
+    '@desktop': { height: '450px' },
+  },
 
   variants: {
     align: {
@@ -52,8 +60,20 @@ export const HeroImage: FunctionComponent<HeroImageProps> = ({
   src,
   alt,
   align,
+  blurhash,
 }) => (
   <HeroImageContainer id="hero-image" align={align as Align}>
+    {blurhash && (
+      <Blurhash
+        className="blurhash"
+        hash={blurhash as string}
+        width="100%"
+        height="450px"
+        resolutionX={32}
+        resolutionY={32}
+        punch={1}
+      />
+    )}
     <HeroImageStyled src={src} alt={alt} />
   </HeroImageContainer>
 )
--- app/routes/now.tsx
+++ app/routes/now.tsx
@@ -16,6 +16,7 @@ export default function Now() {
     heroImage:
       'https://images.unsplash.com/photo-1609207436629-47a022570fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
     heroImageAlign: 'center',
+    heroImageBlurhash: 'LcDwy]IpxuoJ*0ofRPj[E4ofRPWV',
   }
 
   return (
@@ -25,6 +26,7 @@ export default function Now() {
           alt={frontmatter.heroName}
           src={frontmatter.heroImage}
           align={frontmatter.heroImageAlign}
+          blurhash={frontmatter.heroImageBlurhash}
         />
       </Hero>
 
