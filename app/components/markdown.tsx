13d75b0e72f70945302d61a6479f2594e4715004
M Haidar Hanif <git@mhaidarhanif.com>
2022-03-16 06:04:21 +0700

    fix: markdown overrides heading font normal

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -11,12 +11,12 @@ export const Markdown: FunctionComponent<MarkdownProps> = ({ children }) => (
   <MarkdownToJSX
     options={{
       overrides: {
-        h1: { component: H, props: { as: 'h1' } },
-        h2: { component: H, props: { as: 'h2' } },
-        h3: { component: H, props: { as: 'h3' } },
-        h4: { component: H, props: { as: 'h4' } },
-        h5: { component: H, props: { as: 'h5' } },
-        h6: { component: H, props: { as: 'h6' } },
+        h1: { component: H, props: { as: 'h1', font: 'normal' } },
+        h2: { component: H, props: { as: 'h2', font: 'normal' } },
+        h3: { component: H, props: { as: 'h3', font: 'normal' } },
+        h4: { component: H, props: { as: 'h4', font: 'normal' } },
+        h5: { component: H, props: { as: 'h5', font: 'normal' } },
+        h6: { component: H, props: { as: 'h6', font: 'normal' } },
         p: { component: P },
       },
     }}
